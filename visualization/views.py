aa9eaac4ed884f72d95f96d627cab56e22927fac
Olga Kubiszyn <olga@kubiszyn.eu>
2021-11-27 00:16:32 +0100

    formated input file and refactor

--- visualization/views.py
+++ visualization/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 #import numpy as np
 import random as rnd
 from subprocess import Popen
+import copy
 
 def index(request):
     return render(request, 'visualization/index.html')
@@ -15,68 +16,102 @@ def index(request):
 coords = []
 
 def find(request):
-    try:
-        city = request.POST['city']
-        nominatim = Nominatim()
-        place = nominatim.query(city)
-
-        graph_area = (place.displayName())
-        G = ox.graph_from_place(graph_area, network_type='drive')
-
-        G = ox.add_edge_speeds(G)
-        G = ox.add_edge_travel_times(G)
+    #try:
+        G = get_graph(request.POST['city'])
 
         print(len(G.nodes()))
         #print(G.adj)
 
-        adj_list = {}
-        for v in G.adj:
-            adj_list[v] = []
-            for u, value in G.adj[v].items():
-                adj_list[v].append({u : value[0]['length']})
-
-        #print(adj_list)
-
         #fig, ax = ox.plot_graph(G, figsize=(10, 10), node_size=2, edge_color='y', edge_linewidth=0.2)
 
-        shops_nr = rnd.randint(4, 10)
-        #print(G.nodes())
-        shops = rnd.sample(G.nodes(), shops_nr*2)
-        warehouses = shops[len(shops)//2:]
-        shops = shops[:len(shops)//2]
-        print(shops)
-        print(warehouses)
+        shops_nr, shops, warehouses, shops_needs, warehouses_loads = randomize_places(G)
 
-        input_file = open("input.txt", "a")
-        input_file.write(str(adj_list))
-        input_file.write("\n")
-        input_file.write(str(shops))
-        input_file.write(str(warehouses))
-        input_file.close()
+        write_to_file(G, shops, warehouses, shops_needs, warehouses_loads)
 
-        Popen(['./a.out'])
-
-        output_file = open("output.txt", "r")
-        print(output_file.read())
-        output_file.close() 
+        get_results()
 
         global coords
         coords = []
         for i in range(len(shops)):
             print(G.nodes()[shops[i]])
-            coords.append(map(G.nodes()[shops[i]], G.nodes()[warehouses[i]], G, shops[i], warehouses[i]))
+            coords.append(map(G, shops[i], warehouses[i]))
         return render(request, 'visualization/index.html', {'shops_nr' : shops_nr})
         #return render(request, 'visualization/index.html', {})
-    except (TypeError):
-        return render(request, 'visualization/index.html', {
-            'error_message': "Nie podano miasta.",
-        })
+    # except (TypeError):
+    #     return render(request, 'visualization/index.html', {
+    #         'error_message': "Nie podano miasta.",
+    #     })
+
+def get_graph(city):
+    nominatim = Nominatim()
+    place = nominatim.query(city)
+
+    graph_area = (place.displayName())
+    G = ox.graph_from_place(graph_area, network_type='drive')
+
+    G = ox.add_edge_speeds(G)
+    G = ox.add_edge_travel_times(G)
+
+    return G
+
+def randomize_places(G):
+    shops_nr = rnd.randint(4, 10)
+    shops = rnd.sample(G.nodes(), shops_nr*2)
+
+    warehouses = shops[len(shops)//2:] #czy magazynow moze byc wiecej niz sklepow
+    shops = shops[:len(shops)//2]
+
+    print(shops)
+    print(warehouses)
+
+    shops_needs = []
+    warehouses_loads = []
+
+    for i in range(len(shops)):
+        shops_needs.append(rnd.randint(10, 40) * 10)
+    for i in range(len(warehouses)):
+        warehouses_loads.append(rnd.randint(10, 40) * 10)
+    
+    return shops_nr, shops, warehouses, shops_needs, warehouses_loads
+
+def write_to_file(G, shops, warehouses, shops_needs, warehouses_loads):
+    nodes = list(G.nodes()) #copy.deepcopy(list(G.nodes()))
+
+    input_file = open("input.txt", "w")
+    input_file.write(str(len(G.nodes())))
+    input_file.write("\n")
+
+    for v in G.adj:
+        input_file.write(str(nodes.index(v)) + ": ")
+        for u, value in G.adj[v].items():
+            input_file.write(str(nodes.index(u)) + " " + str(value[0]['length']) + " ")
+        input_file.write("\n")
+        
+    input_file.write("---\n")
+    for i in range(len(shops)):
+        input_file.write(str(nodes.index(shops[i])) + " " + str(shops_needs[i]) + " ")
+
+    input_file.write("\n---\n")
+    for i in range(len(warehouses)):
+        input_file.write(str(nodes.index(warehouses[i])) + " " + str(warehouses_loads[i]) + " ")
+    
+    input_file.close()
+
+def get_results():
+    Popen(['./a.out'])
+
+    output_file = open("output.txt", "r")
+    print(output_file.read())
+    output_file.close()
 
 def result(request):
     global coords
     return JsonResponse({"coords" : coords})
 
-def map(place1, place2, G, shop, warehouse):
+def map(G, shop, warehouse):
+
+    place1 = G.nodes()[shop]
+    place2 = G.nodes()[warehouse]
 
     origin_point = (float(place1["x"]), float(place1["y"]))
     destination_point = (float(place2["x"]), float(place2["y"]))
